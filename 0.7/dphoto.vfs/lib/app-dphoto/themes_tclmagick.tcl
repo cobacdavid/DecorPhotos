7b398c55ff6290954a3b2b79b289de39b9c5ec23
cobdav <david.cobac@gmail.com>
2014-05-04 16:50:18 +0200

    my almost first commit -- not fully functionnal

--- 0.7/dphoto.vfs/lib/app-dphoto/themes_tclmagick.tcl
+++ 0.7/dphoto.vfs/lib/app-dphoto/themes_tclmagick.tcl
@@ -1,18 +1,18 @@
 set theme(0) {
     # le logo 
-    pixane scale p logo -to $fa $fa -width $wL -height $hL -quality 9
+    #pixane scale p logo -to $fa $fa -width $wL -height $hL -quality 9
     # ligne horizontale basse
-    pixane line p $fa $Y [expr {$minX-$fa}] $Y -width $(user:tLine)
+    #pixane line p $fa $Y [expr {$minX-$fa}] $Y -width $(user:tLine)
     # ligne verticale gauche
-    pixane line p $fa [expr {2*$fa + $hL}] $fa $Y -width $(user:tLine)
+    #pixane line p $fa [expr {2*$fa + $hL}] $fa $Y -width $(user:tLine)
     # ligne verticale droite
-    pixane line p [expr {$w-$Xoff}] $Y [expr {$w-$Xoff}] $minY -width $(user:tLine)
+    #pixane line p [expr {$w-$Xoff}] $Y [expr {$w-$Xoff}] $minY -width $(user:tLine)
 }
 set docTheme(0) {logo en haut à gauche + lignes verticale et horizontale + lignes de textes}
 
 set theme(1) {
     # le logo 
-    pixane scale p logo -to $fa $fa -width $wL -height $hL -quality 9
+    #pixane scale p logo -to $fa $fa -width $wL -height $hL -quality 9
 }
 set docTheme(1) {logo seul en haut à gauche + lignes de textes}
 
